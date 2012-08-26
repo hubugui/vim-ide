3a0239680ab000b3d53b5544cde4b4c9028de931
wangyang <hubugui@gmail.com>
2012-08-26 22:45:14 +0800

    grep server name

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+vim-ide is a vim pluging environment, includes ctags,taglist,cscope,nerdtree,omnicppcomplete,supertab.
+
 run 'install.sh' will auto install vim-ide.
--- install.sh
+++ install.sh
@@ -5,32 +5,45 @@ touch ~/.vimrc
 
 #system clipboard
 sudo apt-get install vim-gnome
-
 #nerdtree
 unzip nerdtree.zip -d ~/.vim
-
 #ctags
 sudo apt-get install exuberant-ctags
-
 #taglist
 unzip taglist_45.zip -d ~/.vim
-
 #cscope
 sudo apt-get install cscope
 wget http://cscope.sourceforge.net/cscope_maps.vim -O ~/.vim/plugin/cscope_maps.vim
-
-vim -es --servername VIM_IDE &
-
 #omnicppcompelete
 unzip omnicppcomplete-0.41.zip -d ~/.vim 
 
+#vim server
+server=VIM_IDE_FISH
+vim -es --servername $server&
+
+echo "waiting for $server"
+
+while true
+do
+	name=`vim --serverlist | grep $server`
+	if [ "$server" = "$name" ]; then
+		echo "waiting end, vim server started"
+	    break
+	else
+		echo "waiting, name=$name"
+		sleep 1
+	fi
+done
+
 #supertab
-vim --servername VIM_IDE --remote supertab.vmb
-vim --servername VIM_IDE --remote-send '<C-\><C-N>:so %<CR>'
-
+vim --servername $server --remote supertab.vmb
+vim --servername $server --remote-send '<C-\><C-N>:so %<CR>'
 #refresh doc
-vim --servername VIM_IDE --remote ~/.vim/doc
-vim --servername VIM_IDE --remote-send '<C-\><C-N>:helptags.<CR>'
+vim --servername $server --remote ~/.vim/doc
+vim --servername $server --remote-send '<C-\><C-N>:helptags.<CR>'
 
-vim --servername VIM_IDE --remote-send '<C-\><C-N>:q<CR>'
+vim --servername $server --remote-send '<C-\><C-N>:q<CR>'
+
 cat .vimrc >> ~/.vimrc
+
+echo "install complete"
